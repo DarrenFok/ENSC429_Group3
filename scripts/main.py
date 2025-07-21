5db53fdce902b45308b32444379818b7e2986b6f
Chris <55999720+chis456@users.noreply.github.com>
2025-07-21 14:10:46 -0700

    Created script to STFT audio file to spectrogram
    yay
    
    working mp3 test
    
    Working STFT and audio reconstruction tests
    
    yay
    
    working mp3 test
    
    refactor file organizaiton
    
    [#2] working audio to STFT script
    
    refactored code in a new file
    
    further segementation

---
+++ scripts/main.py
@@ -0,0 +1,30 @@
+import os
+
+import librosa
+import numpy as np
+import getSTFT
+
+def convert_all_files():
+
+    directory = os.fsencode('../data/Samples')
+
+    # TAKES A REALLY LONG TIME FOR LONGER AUDIO FILES
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        getSTFT.convert_to_excel(filename, 0)
+
+def convert_one(track_title):
+
+    directory = os.fsencode('../data/Samples')
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if track_title == filename:
+            getSTFT.convert_to_excel(filename, 1)
+            return
+
+def main():
+    print("testmain")
+    convert_one("sunflower-street-drumloop.mp3")
+
+main()
